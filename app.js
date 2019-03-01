a9aa31437c22acd3749248420f953f928f2e8d9e
Karunagaran Ulaganathan <karunagu@amazon.com>
2019-03-01 13:42:26 -0800

    Added first set of files

---
+++ app.js
@@ -0,0 +1,30 @@
+
+var firstandpike = {
+    minNumCust: 23,
+    maxNumCust: 65,
+    avgCookieSale: 6.3
+}
+
+var seatac = {
+    minNumCust : 3,
+    maxNumCust : 24,
+    avgCookieSale : 1.2
+}
+
+var seattlecenter = {
+    minNumCust : 11,
+    maxNumCust : 38,
+    avgCookieSale : 3.7
+}
+
+var capitolhill= {
+    minNumCust : 20,
+    maxNumCust : 38,
+    avgCookieSale : 2.3
+}
+
+var alki = {
+    minNumCust : 2,
+    maxNumCust : 16,
+    avgCookieSale : 4.6
+}
