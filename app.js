270d989b044457e54057cc9d4f76b506a5394108
Karunagaran Ulaganathan <karunagu@amazon.com>
2019-03-08 15:48:58 -0800

    completed all totals

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-    // ,seatac,seattlecenter,capitolhill,alki]
 var table = document.getElementById('shell');    
 
 function Stores (name,minNumCust,maxNumCust,avgCookieSale){
@@ -11,16 +10,15 @@ function Stores (name,minNumCust,maxNumCust,avgCookieSale){
 }    
 var stores = [];
 var rowData = [];
+var hourTotal = [];
 
 var firstandpike = new Stores('1st and Pike',23,65,6.3);
-// console.log(firstandpike);
 var seatac = new Stores('SeaTac',3,24,1.2);
 var seattlecenter = new Stores('Seattle Center',11,38,3.7);
 var capitolhill = new Stores('Capitol Hill',20,38,2.3);
 var alki = new Stores('Alki',2,16,4.6);
 stores.push(firstandpike,seattlecenter,capitolhill,alki);
 
-// var stores = [firstandpike,seatac,seattlecenter,capitolhill,alki];    
 function getCustCount(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -46,38 +44,18 @@ function getCookieCount(numCust,avgCookies) {
       totCookieStore = totCookieStore + numCookies;
       rowData.push(
         '<td>' + numCookies + '</td>' 
-         )
+         ) 
+         if (isNaN(hourTotal[i])) {
+              hourTotal[i] = numCookies ;
+         }
+         else {
+              hourTotal[i] = hourTotal[i] + numCookies ;
+         }
     }
     return totCookieStore;
     // console.log(rowData);    
   }
-
-  //     // var tdTabHead = document.createElement('td');
-  //     // console.log(storetime);
-  //     // tdTabHead.textContent = storetime;
-  //     // tabHead.appendChild(tdTabHead);
-  
-  //     // libTabHead.tabHead = storetime;
-  //     // tabHead.appendChild(libTabHead);
-  //     // 1. Create new element
-  //     var liEl = document.createElement('li');
-  
-  //     // 2. Give the element some content
-
-  //     liEl.textContent = storetime + ': ' + numCookies + ' Cookies';
-  //     // console.log('StoreCount',storeCount);
-  
-  //     // console.log('stores[storeCount].minNumCust',stores[storeCount].minNumCust);
-  //     // console.log ('numCust', numCust);
-  //     // console.log ('numCookies', numCookies);
-  
-  //     // 3. Append the new element to its parent in the DOM
-  //     StoreList.appendChild(liEl);
-  //     totCookies = totCookies + numCookies;
-
-  //   }
-  // }
-  
+ 
   function render(tableRow) {
     for (var j=0; j < tableRow.length; j++) {
       var newRow = document.createElement('td');
@@ -113,10 +91,7 @@ function getCookieCount(numCust,avgCookies) {
 
   
   var storeCount = 0;
-  // console.log('store Count', storeCount);
-  // console.log('stores.length' ,stores.length);
   while (storeCount < stores.length) {
-      // console.log(stores[storeCount].name);
     var totCookies = 0;
       var rowData = [];
       rowData.push(
@@ -127,61 +102,34 @@ function getCookieCount(numCust,avgCookies) {
       rowData.push(
         '<td>' + totRow + '</td>' 
          )
-      // console.log(rowData);
       render(rowData);
       var newRow = document.createElement('tr');
       newRow.innerHTML = ' ';      
       table.appendChild(newRow);
-
-//   liEl.textContent = 'Total : ' + totCookies + ' Cookies';  
     storeCount = storeCount + 1;
+
+
+
 }
 
-//     while (storeCount < stores.length) {
+var rowData = [];
+var grandTotal = 0;
+rowData.push(
+  '<td>' + 'Total'+ '</td>' 
+   )  
+for (var i = 6; i <= 19; i++) {
 
-//     var totCookies = 0;
+  rowData.push(
+    '<td>' + hourTotal[i]+ '</td>' 
+     )
+    grandTotal = grandTotal + hourTotal[i];
+}
+rowData.push(
+  '<td>' + grandTotal+ '</td>' 
+   )
+render(rowData);
+var newRow = document.createElement('tr');
+newRow.innerHTML = ' ';      
+table.appendChild(newRow);
 
-//     var liEl = document.createElement('li');
-
-//     liEl.textContent = 'Store Name : ' + stores[storeCount].name;
-//     StoreList.appendChild(liEl);
-// //   console.log('StoreCount',storeCount);
-
-//   for (var i = 6; i <= 20; i++) {
-//     if (i < 12) {
-//         storetime = i + ' am' } 
-//     else if (i===12) {
-//         storetime = i + ' pm' 
-//     }
-//     else if (i > 12) {
-//         storetime = (i-12) + ' pm' 
-//     }
-//     // var tdTabHead = document.createElement('td');
-//     // console.log(storetime);
-//     // tdTabHead.textContent = storetime;
-//     // tabHead.appendChild(tdTabHead);
-
-//     // libTabHead.tabHead = storetime;
-//     // tabHead.appendChild(libTabHead);
-//     // 1. Create new element
-//     var liEl = document.createElement('li');
-
-//     // 2. Give the element some content
-//     numCust = getCustCount(stores[storeCount].minNumCust,stores[storeCount].maxNumCust);
-//     numCookies = Math.ceil(numCust * stores[storeCount].avgCookieSale);
-//     liEl.textContent = storetime + ': ' + numCookies + ' Cookies';
-//     // console.log('StoreCount',storeCount);
-
-//     // console.log('stores[storeCount].minNumCust',stores[storeCount].minNumCust);
-//     // console.log ('numCust', numCust);
-//     // console.log ('numCookies', numCookies);
-
-//     // 3. Append the new element to its parent in the DOM
-//     StoreList.appendChild(liEl);
-//     totCookies = totCookies + numCookies;
-//   }
-//   liEl.textContent = 'Total : ' + totCookies + ' Cookies';  
-//   storeCount = storeCount + 1;
-// }
-// //   console.log (stores[0],stores[0].minNumCust,stores[0].maxNumCust,stores[0].avgCookies);
 
